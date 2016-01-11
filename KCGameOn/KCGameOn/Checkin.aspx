c57acc2dc7e0ec77f0b4e38772b3c756161743d4
Shane Faulkner <contact@shanefaulkner.com>
2016-01-10 22:30:56 -0600

    Beginnings of Check-In Page
    Dirty working check-in page.  Needs improvements.

---
+++ KCGameOn/KCGameOn/Checkin.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Checkin.aspx.cs" Inherits="KCGameOn.Checkin" %>
+
+<%@ Import Namespace="KCGameOn" %>
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+
+    <asp:Label ID="paidStatus" runat="server" Visible="false" />
+    <asp:Label ID="checkInStatus" runat="server" Visible="false" />
+    <%if (!String.IsNullOrEmpty(SessionVariables.UserName))
+      {%>
+    <div class="container">
+
+        <%if (this.paidStatus.Text == "N")
+        {%>
+            <%if (this.checkInStatus.Text == "False")
+            {%>
+                <form id="CheckinForm" runat="server">
+                    <asp:Label ID="successLabel" Text="Click the button to check yourself in." runat="server" /><br />
+                    <asp:Button ID="CheckinButton" CssClass="btn btn-inverse" OnClick="CheckinButton_Click" runat="server" Text="Checkin"/>
+                </form>
+            <%}
+            else
+            {%>
+                You already checked in.
+            <%} %>
+        <%}
+        else
+        {%>
+            You have not paid.  Please pay so you can check in.
+        <%} %>    
+    </div>
+    <%}
+    else
+    {%>
+        <h2>Please <a href="/Account/Login.aspx">login</a> to view this page.</h2>
+        <h3>There are currently <%=count.ToString() %> people registered at kcgameon.com, <a href="https://www.kcgameon.com/Account/Register.aspx">sign up</a> today!</>
+        <br />
+    <%} %>
+</asp:Content>
---
+++ KCGameOn/KCGameOn/Checkin.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Text;
+
+namespace KCGameOn
+{
+    public partial class Checkin : System.Web.UI.Page
+    {
+        public static int count;
+        public static StringBuilder UserHTML;
+        public String hasPaid;
+        public bool hasCheckedIn;
+        String connectionString = ConfigurationManager.ConnectionStrings["KcGameOnSQL"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT verifiedPaid FROM payTable WHERE username = \'" + SessionVariables.UserName + "\'", new MySqlConnection(connectionString));
+                cmd.Connection.Open();
+                hasPaid = (String) cmd.ExecuteScalar();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                paidStatus.Text = hasPaid.ToString();
+
+                MySqlCommand cmd2 = new MySqlCommand("SELECT checkedin FROM seatingchart WHERE username = \'" + SessionVariables.UserName + "\'", new MySqlConnection(connectionString));
+                cmd2.Connection.Open();
+                hasCheckedIn = (bool)cmd2.ExecuteScalar();
+                cmd2.ExecuteNonQuery();
+                cmd2.Connection.Close();
+                checkInStatus.Text = hasCheckedIn.ToString();
+            }
+            catch { }
+        }
+
+        protected void CheckinButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("UPDATE seatingchart SET checkedin = true WHERE Username = \'" + SessionVariables.UserName + "\'", new MySqlConnection(connectionString));
+                cmd.Connection.Open();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                successLabel.Text = "You have successfully checked yourself in.";
+            }
+            catch {
+                successLabel.Text = "An error occured.  Sorry please try again.";
+            }
+        }
+    }
+}
